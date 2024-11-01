5cd131cf337a0fd49e987de093397453c73b3020
dafahfabiansyah <davarobiansyah@gmail.com>
2024-11-01 21:54:53 +0700

    get data from backend in city card

--- src/components/CarouselCitySection.tsx
+++ src/components/CarouselCitySection.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import CityCard from "./CityCard";
+import { City } from "../types/type";
+import axios from "axios";
 
 const CarouselCitySection = () => {
+  const [cities, setCities] = useState<City[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<String | null>(null);
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_API_URL}/api/cities`, {
+      headers: {
+        "x-api-key": import.meta.env.VITE_API_KEY,
+      },
+    })
+      .then((response) => {
+        setCities(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Failed load data : {error}</p>;
+  }
+
   return (
     <section id="Cities" className="flex flex-col gap-[30px] mt-[100px]">
       <div className="w-full max-w-[1130px] mx-auto flex items-center justify-between">
@@ -26,9 +54,11 @@ const CarouselCitySection = () => {
             slidesOffsetAfter={30}
             slidesOffsetBefore={30}
           >
-            <SwiperSlide className="!w-fit first-of-type:pl-[calc((100%-1130px-60px)/2)] last-of-type:pr-[calc((100%-1130px-60px)/2)]">
-              <CityCard />
-            </SwiperSlide>
+            {cities.map((city) => (
+              <SwiperSlide key={city.id} className="!w-fit first-of-type:pl-[calc((100%-1130px-60px)/2)] last-of-type:pr-[calc((100%-1130px-60px)/2)]">
+                <CityCard city={city} />
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </div>
---
+++ src/types/type.ts
@@ -0,0 +1,44 @@
+export interface Office {
+    id: number;
+    name: string;
+    slug: string;
+    images: Image[];
+    thumbnail: string;
+    price: number
+    benefits: Benefit[]
+    duration: number
+    description: string
+    city: City
+}
+interface Image {
+    id: number;
+    name: string;
+}
+
+interface Benefit {
+    id: number;
+    name: string;
+}
+
+export interface City {
+    id: number;
+    name: string;
+    slug: string;
+    image: string;
+    // officeSpace_count : number;
+    officeSpaceCount : number;
+    officeSpace : Office[];
+}
+
+export interface BookingDetails {
+    id : number;
+    name : string;
+    // phone_number : number;
+    phoneNumber : number;
+    // booking_trx_id : string;
+    bookingTransactionId : string;
+    // is_paid : boolean;
+    isPaid : boolean;
+    office : Office;
+}
+
